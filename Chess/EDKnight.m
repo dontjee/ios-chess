c604cdfb3af3f063336e1c40317bcc1016547df7
Evan Dontje <dontjee@gmail.com>
2013-05-15 13:28:18 -0400

    Handling Knight moving over pieces

--- Chess/EDKnight.m
+++ Chess/EDKnight.m
@@ -7,6 +7,7 @@
 //
 
 #import "EDKnight.h"
+#import "EDChessGame.h"
 
 @implementation EDKnight
 - (BOOL) canMoveToPosition: (EDChessPoint*) position
@@ -17,7 +18,39 @@
     BOOL isAValidMovePartTwo = abs(position.XPosition - self.position.XPosition) == 2
                                 && abs(position.YPosition - self.position.YPosition) == 1;
     
-    return isAValidMovePartOne || isAValidMovePartTwo;
+    BOOL canMove = isAValidMovePartOne || isAValidMovePartTwo;
+    
+    EDChessPoint* straightLineEndPositionOne;
+    if( abs(position.XPosition - self.position.XPosition) == 2 )
+    {
+        //Add one to ensure we are checking the 3rd square
+        int endXPosition = position.XPosition > self.position.XPosition ? position.XPosition + 1 : position.XPosition - 1;
+        int endYPosition = self.position.YPosition;
+        straightLineEndPositionOne = [EDChessPoint pointWithPositionString:[NSString stringWithFormat:@"%i%i",endXPosition,endYPosition]];
+    }
+    else
+    {
+        int endXPosition = self.position.XPosition;
+        //Add one to ensure we are checking the 3rd square
+        int endYPosition = position.YPosition > self.position.YPosition ? position.YPosition + 1 : position.YPosition - 1;
+        straightLineEndPositionOne = [EDChessPoint pointWithPositionString:[NSString stringWithFormat:@"%i%i",endXPosition,endYPosition]];
+    }
+    int piecesCrossedOne = [self.game getCountOfPiecesCrossedFrom: self.position toPosition: straightLineEndPositionOne];
+    
+    EDChessPoint* straightLineStartPositionTwo;
+    if( abs(position.XPosition - self.position.XPosition) == 2 )
+    {
+        int startXPosition = position.XPosition > self.position.XPosition ? self.position.XPosition - 1 : self.position.XPosition + 1;
+        straightLineStartPositionTwo = [EDChessPoint pointWithPositionString:[NSString stringWithFormat:@"%i%i",startXPosition,position.YPosition]];
+    }
+    else
+    {
+        int startYPosition = position.YPosition > self.position.YPosition ? self.position.YPosition - 1 : self.position.YPosition + 1;
+        straightLineStartPositionTwo = [EDChessPoint pointWithPositionString:[NSString stringWithFormat:@"%i%i",position.XPosition,startYPosition]];
+    }
+    int piecesCrossedTwo = [self.game getCountOfPiecesCrossedFrom: straightLineStartPositionTwo toPosition: position];
+    
+    return canMove && (piecesCrossedOne <= 1 || piecesCrossedTwo <= 1);
 }
 
 - (NSString*) getTextRepresentingPiece
